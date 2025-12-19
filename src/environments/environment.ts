30d8ef8740cc4656593acc8145239996ea220e5b
Rico Apon <rico.apon@gmail.com>
2025-12-19 20:16:33 +0100

    Add firebase environment information

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDSBX5vX3ivXbPwy_Qe6odB_kR9Hcd9BAg",
+    authDomain: "woodpecker-method.firebaseapp.com",
+    projectId: "woodpecker-method",
+    storageBucket: "woodpecker-method.firebasestorage.app",
+    messagingSenderId: "348292288398",
+    appId: "1:348292288398:web:5efaf1bc91c57be2721edf",
+    measurementId: "G-TCTKS4TEV9"
+  }
+};
+
+
