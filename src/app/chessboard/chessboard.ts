3c31a5ae2a28624860b8c61f1225321aefed3302
Rico Apon <rico.apon@gmail.com>
2025-12-20 10:22:17 +0100

    Install chess.js to determine possible moves

--- src/app/chessboard/chessboard.ts
+++ src/app/chessboard/chessboard.ts
@@ -1,6 +1,8 @@
 import {AfterViewInit, Component, ElementRef, ViewChild, ViewEncapsulation} from '@angular/core';
-import { Chessground } from '@lichess-org/chessground';
+import {Chessground} from '@lichess-org/chessground';
 import {Config} from '@lichess-org/chessground/config';
+import {Key} from '@lichess-org/chessground/types';
+import {Chess, SQUARES} from 'chess.js'
 
 @Component({
   selector: 'app-chessboard',
@@ -10,19 +12,33 @@ import {Config} from '@lichess-org/chessground/config';
   standalone: true,
   encapsulation: ViewEncapsulation.None
 })
-export class Chessboard implements AfterViewInit  {
+export class Chessboard implements AfterViewInit {
   @ViewChild('boardContainer') boardEl!: ElementRef<HTMLDivElement>;
 
   ngAfterViewInit() {
+    const chess = new Chess('1r1n1rk1/b1p1qppp/p2p1n2/4pP2/P3P3/2PPNB2/6PP/R1BQK2R w KQ - 0 16');
+    const dests = new Map<Key, Key[]>();
+    SQUARES.forEach(sq => {
+      const moves = chess.moves({ square: sq, verbose: true });
+      if (moves.length > 0) {
+        dests.set(sq, moves.map(m => m.to));
+      }
+    });
+
     const config: Config = {
-      fen: 'start',
+      fen: chess.fen(),
       orientation: 'white',
       draggable: {
         enabled: true
-      }
+      },
+      movable: {
+        free: false,
+        showDests: true,
+        dests: dests
+      },
     };
 
-    // create the board inside the container
+    // Create the board inside the container.
     Chessground(this.boardEl.nativeElement, config);
   }
 }
